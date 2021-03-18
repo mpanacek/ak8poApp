3a2ac0795a26068c5e6834e3ca956dd47a58945e
Martin Panáček <martin.panacek44@gmail.com>
2021-03-18 16:43:44 +0100

    Project for DB migration added + connection to the database

---
+++ SecretaryApp/SecretaryApp.EntityFramework/Migrations/20210318154050_Initial.cs
@@ -0,0 +1,206 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecretaryApp.EntityFramework.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FullName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonalEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkPhone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonalPhone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WorkPoints_NoEng = table.Column<int>(type: "int", nullable: false),
+                    WorkPoints = table.Column<int>(type: "int", nullable: false),
+                    WorkingTime = table.Column<double>(type: "float", nullable: false),
+                    DoctoralStudent = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subjects",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NumberOfCredits = table.Column<int>(type: "int", nullable: false),
+                    NumberOfWeeks = table.Column<int>(type: "int", nullable: false),
+                    HoursOfLectures = table.Column<int>(type: "int", nullable: false),
+                    HoursOfExcercises = table.Column<int>(type: "int", nullable: false),
+                    ClassSize = table.Column<int>(type: "int", nullable: false),
+                    WayOfCompletion = table.Column<int>(type: "int", nullable: false),
+                    Language = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subjects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WeightsOfWorkPoints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Lecture = table.Column<double>(type: "float", nullable: false),
+                    Excercise = table.Column<double>(type: "float", nullable: false),
+                    Seminar = table.Column<double>(type: "float", nullable: false),
+                    Lecture_Eng = table.Column<double>(type: "float", nullable: false),
+                    Excercise_Eng = table.Column<double>(type: "float", nullable: false),
+                    Seminar_Eng = table.Column<double>(type: "float", nullable: false),
+                    Credit = table.Column<double>(type: "float", nullable: false),
+                    ClassifiedCredit = table.Column<double>(type: "float", nullable: false),
+                    Exam = table.Column<double>(type: "float", nullable: false),
+                    Credit_Eng = table.Column<double>(type: "float", nullable: false),
+                    ClassifiedCredit_Eng = table.Column<double>(type: "float", nullable: false),
+                    Exam_Eng = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeightsOfWorkPoints", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Groups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubjectId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    Shortcut = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Class = table.Column<int>(type: "int", nullable: false),
+                    NumberOfStudents = table.Column<int>(type: "int", nullable: false),
+                    Semester = table.Column<int>(type: "int", nullable: false),
+                    StudyForm = table.Column<int>(type: "int", nullable: false),
+                    StudyType = table.Column<int>(type: "int", nullable: false),
+                    Language = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Groups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Groups_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WorkLabels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeId = table.Column<int>(type: "int", nullable: true),
+                    SubjectId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    LectureType = table.Column<int>(type: "int", nullable: false),
+                    NumberOfStudents = table.Column<int>(type: "int", nullable: false),
+                    NumberOfHours = table.Column<int>(type: "int", nullable: false),
+                    NumberOfWeeks = table.Column<int>(type: "int", nullable: false),
+                    Language = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WorkLabels", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_WorkLabels_Employees_EmployeeId",
+                        column: x => x.EmployeeId,
+                        principalTable: "Employees",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_WorkLabels_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SubjectGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SubjectId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    GroupId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SubjectGroups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SubjectGroups_Groups_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "Groups",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_SubjectGroups_Subjects_SubjectId",
+                        column: x => x.SubjectId,
+                        principalTable: "Subjects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Groups_SubjectId",
+                table: "Groups",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectGroups_GroupId",
+                table: "SubjectGroups",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SubjectGroups_SubjectId",
+                table: "SubjectGroups",
+                column: "SubjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkLabels_EmployeeId",
+                table: "WorkLabels",
+                column: "EmployeeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WorkLabels_SubjectId",
+                table: "WorkLabels",
+                column: "SubjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SubjectGroups");
+
+            migrationBuilder.DropTable(
+                name: "WeightsOfWorkPoints");
+
+            migrationBuilder.DropTable(
+                name: "WorkLabels");
+
+            migrationBuilder.DropTable(
+                name: "Groups");
+
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Subjects");
+        }
+    }
+}
---
+++ SecretaryApp/SecretaryApp.EntityFramework/SecretaryAppDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace SecretaryApp.EntityFramework
+{
+    class SecretaryAppDbContextFactory : IDesignTimeDbContextFactory<SecretaryAppDbContext>
+    {
+        public SecretaryAppDbContext CreateDbContext(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<SecretaryAppDbContext>();
+            options.UseSqlServer("Server = (localdb)\\MSSQLLocalDB; Database = SecretaryApp; Trusted_Connection = True;");
+
+            return new SecretaryAppDbContext(options.Options);
+        }
+    }
+}
