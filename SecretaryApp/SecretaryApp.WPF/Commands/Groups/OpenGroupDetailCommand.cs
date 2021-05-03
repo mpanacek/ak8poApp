b226e264f77e5493e836e25edb21ae36749031a5
Martin Panáček <martin.panacek44@gmail.com>
2021-05-03 20:37:29 +0200

    group detail command cleanup

--- SecretaryApp/SecretaryApp.WPF/Commands/Groups/OpenGroupDetailCommand.cs
+++ SecretaryApp/SecretaryApp.WPF/Commands/Groups/OpenGroupDetailCommand.cs
@@ -1,10 +1,6 @@
 ﻿using SecretaryApp.Domain.Models;
 using SecretaryApp.WPF.ViewModels;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 
 namespace SecretaryApp.WPF.Commands.Groups
